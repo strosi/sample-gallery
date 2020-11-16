a9d771f032c70a78f3556c8f494437edb9763934
strosi <stoeva.rositsa@gmail.com>
2020-11-16 14:38:33 +0200

    change js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const btn = document.querySelector('button');
 const subGallery = document.querySelector('.subgallery');
 const subGalleryImgs = document.querySelector('.subgallery .project-images');
 const textHolder = document.querySelector('.description .text');
-console.log(textHolder);
 
 function closeSubGallery() {
     subGalleryImgs.innerHTML = '';
@@ -22,16 +21,39 @@ function openSubGallery(targetImg) {
     subGalleryImgs.append(newImg);
 }
 
-async function loadSubGalleryText(targetImg) {
+// async function loadSubGalleryText(targetImg) {
+//     const descriptText = document.createElement('p');
+//     const textPath = targetImg.substring(0, targetImg.lastIndexOf('/') + 1);
+
+//     let response = await fetch(textPath + 'description.txt')
+//         .catch(err => console.error('Text not found'));
+
+//     if (response.ok) {
+//         response.text().then(data => {
+//             descriptText.innerHTML = data;
+//             textHolder.append(descriptText);
+//         });
+
+//         // textHolder.append(descriptText);
+//     }
+// }
+
+function loadSubGalleryText(targetImg) {
     const descriptText = document.createElement('p');
     const textPath = targetImg.substring(0, targetImg.lastIndexOf('/') + 1);
 
-    let response = await fetch(textPath + 'description.txt');
-
-    if (response.ok) {
-        descriptText.innerHTML = response.text();
-        textHolder.append(descriptText);
-    }
+    fetch(textPath + 'description.txt')
+        .then(function (response) {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            return response;
+        }).then(function (response) {
+            response.text().then(data => {
+                descriptText.innerHTML = data;
+                textHolder.append(descriptText);
+            });
+        }).catch(err => console.log('I cought the error!'));
 }
 
 // Handle img click
@@ -39,16 +61,10 @@ window.addEventListener('click', function (event) {
     if (!subGallery.classList.contains('active')) {
         const el = event.target;
         if (el.tagName === 'IMG') {
-            // subGallery.classList.add('active');
-            // subGalleryInnerHolder.classList.add('opened');
             const bigImgName = el.getAttribute('src').replace('.jpg', '-big.jpg');
             openSubGallery(bigImgName);
             loadSubGalleryText(bigImgName);
-            // const newImg = document.createElement('img');
-            // newImg.setAttribute('src', bigImgName);
-            // subGalleryInnerHolder.append(newImg);
 
-            // const textHolder = document.querySelector('.description.text');
             // const descriptText = document.createElement('p');
             // const textPath = bigImgName.substring(0, bigImgName.lastIndexOf('/') + 1);
 
