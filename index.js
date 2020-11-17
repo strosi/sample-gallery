561ba25f6351c5e63fa12b2b10a0b686e1445ba9
strosi <stoeva.rositsa@gmail.com>
2020-11-17 15:03:37 +0200

    Create subgallery navigation; Fetch json file

--- index.js
+++ index.js
@@ -1,83 +1,119 @@
 const body = document.querySelector('body');
-const btn = document.querySelector('button');
+const mainGallery = document.querySelector('.flex-gallery');
+const prevBtn = document.querySelector('.prev');
+const closeBtn = document.querySelector('.close');
+const nextBtn = document.querySelector('.next');
 const subGallery = document.querySelector('.subgallery');
 const subGalleryImgs = document.querySelector('.subgallery .project-images');
+let currentImg = '';
+const titleHolder = document.querySelector('.description .title');
 const textHolder = document.querySelector('.description .text');
 
-function closeSubGallery() {
-    subGalleryImgs.innerHTML = '';
-    textHolder.innerHTML = '';
-    subGallery.classList.remove('active');
-    body.classList.remove('noscroll');
+function setSubGalleryNavi(targetImg) {
+    if (targetImg === mainGallery.firstElementChild.firstElementChild) {
+        prevBtn.classList.add('noActiveBtn');
+    }
+
+    if (targetImg === mainGallery.lastElementChild.firstElementChild) {
+        nextBtn.classList.add('noActiveBtn');
+    }
 }
 
-btn.addEventListener('click', closeSubGallery);
-
-function openSubGallery(targetImg) {
-    body.classList.add('noscroll');
-    subGallery.classList.add('active');
+function loadSubGalleryImages(targetImg) {
+    currentImg = targetImg;
+    const bigImgName = targetImg.getAttribute('src').replace('.jpg', '-big.jpg');
     const newImg = document.createElement('img');
-    newImg.setAttribute('src', targetImg);
+    newImg.setAttribute('src', bigImgName);
     subGalleryImgs.append(newImg);
 }
 
-// async function loadSubGalleryText(targetImg) {
-//     const descriptText = document.createElement('p');
-//     const textPath = targetImg.substring(0, targetImg.lastIndexOf('/') + 1);
-
-//     let response = await fetch(textPath + 'description.txt')
-//         .catch(err => console.error('Text not found'));
-
-//     if (response.ok) {
-//         response.text().then(data => {
-//             descriptText.innerHTML = data;
-//             textHolder.append(descriptText);
-//         });
-
-//         // textHolder.append(descriptText);
-//     }
-// }
-
 function loadSubGalleryText(targetImg) {
+    const clientName = document.createElement('h2');
+    const projectType = document.createElement('p');
     const descriptText = document.createElement('p');
-    const textPath = targetImg.substring(0, targetImg.lastIndexOf('/') + 1);
+    const bigImgName = targetImg.getAttribute('src').replace('.jpg', '-big.jpg');
+    const textPath = bigImgName.substring(0, bigImgName.lastIndexOf('/') + 1);
 
-    fetch(textPath + 'description.txt')
+    fetch(textPath + 'description.json')
         .then(function (response) {
             if (!response.ok) {
                 throw new Error(response.statusText);
             }
             return response;
         }).then(function (response) {
-            response.text().then(data => {
-                descriptText.innerHTML = data;
+            response.json().then(data => {
+                clientName.innerHTML = data.client;
+                projectType.innerHTML = data.project;
+                descriptText.innerHTML = data.description;
+                titleHolder.append(clientName);
+                titleHolder.append(projectType);
                 textHolder.append(descriptText);
             });
-        }).catch(err => console.log('I cought the error!'));
+        }).catch(err => console.log(err));
 }
 
-// Handle img click
-window.addEventListener('click', function (event) {
+function setSubGalleryContent(targetImg) {
+    setSubGalleryNavi(targetImg);
+    loadSubGalleryImages(targetImg);
+    loadSubGalleryText(targetImg);
+}
+
+function openSubGallery(targetImg) {
+    body.classList.add('noscroll');
+    subGallery.classList.add('active');
+    setSubGalleryContent(targetImg);
+}
+
+// Handle img click in main gallery
+mainGallery.addEventListener('click', function (event) {
     if (!subGallery.classList.contains('active')) {
         const el = event.target;
         if (el.tagName === 'IMG') {
-            const bigImgName = el.getAttribute('src').replace('.jpg', '-big.jpg');
-            openSubGallery(bigImgName);
-            loadSubGalleryText(bigImgName);
-
-            // const descriptText = document.createElement('p');
-            // const textPath = bigImgName.substring(0, bigImgName.lastIndexOf('/') + 1);
-
-            // fetch(textPath + 'description.txt')
-            //     .then(Response => Response.text())
-            //     .then(data => descriptText.innerText = data);
-            // subGalleryInnerHolder.append(descriptText);
-
-            // let response = await fetch(textPath + 'description.txt');
-            // if (response.ok) {
-            //     descriptText.innerHTML = response.text();
-            //     textHolder.append(descriptText);
-            // }
+            openSubGallery(el);
         }
+    }
+});
+
+function removeSubGalleryContent() {
+    subGalleryImgs.innerHTML = '';
+    titleHolder.innerHTML = '';
+    textHolder.innerHTML = '';
+}
+
+function closeSubGallery() {
+    prevBtn.classList.remove('noActiveBtn');
+    nextBtn.classList.remove('noActiveBtn');
+    removeSubGalleryContent();
+    subGallery.classList.remove('active');
+    body.classList.remove('noscroll');
+}
+
+function previousImageSubgallery() {
+    const prevImg = currentImg.parentElement.previousElementSibling.firstElementChild;
+    removeSubGalleryContent();
+    setSubGalleryContent(prevImg);
+    nextBtn.classList.remove('noActiveBtn');
+}
+
+function nextImageSubgallery() {
+    const nextImg = currentImg.parentElement.nextElementSibling.firstElementChild;
+    removeSubGalleryContent();
+    setSubGalleryContent(nextImg);
+    prevBtn.classList.remove('noActiveBtn');
+}
+
+prevBtn.addEventListener('click', function (event) {
+    if (event.target.classList.contains('noActiveBtn')) { return; }
+    previousImageSubgallery();
+});
+closeBtn.addEventListener('click', closeSubGallery);
+nextBtn.addEventListener('click', function (event) {
+    if (event.target.classList.contains('noActiveBtn')) { return; }
+    nextImageSubgallery();
+});
+
+window.addEventListener('click', function(event) {
+    if(event.target === subGallery) {
+        closeSubGallery();
     }
 });
